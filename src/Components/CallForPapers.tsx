927be15daf55c96b69886c96e8d101201279006e
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2024-03-13 08:32:16 +0530

    Submit paper updated

--- src/Components/CallForPapers.tsx
+++ src/Components/CallForPapers.tsx
@@ -74,7 +74,9 @@ const CallForPapers = () => {
               className="inline-flex items-center justify-center px-5 py-2 mr-3 text-base font-medium text-center text-white rounded-lg bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 no-underline dark:focus:ring-red-900"
               href="https://cmt3.research.microsoft.com/ICAC2N2024"
             >
-              <Link to="/extra">Submit a Paper</Link>
+              <Link to="https://cmt3.research.microsoft.com/ICAC2N2024">
+                Submit a Paper
+              </Link>
 
               <svg
                 className="w-5 h-5 ml-2 -mr-1"
