6b585840edb6f17025396b5b4457aface20a1256
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2024-12-04 17:11:19 +0530

    Speaker

--- src/Components/Speakers.tsx
+++ src/Components/Speakers.tsx
@@ -231,6 +231,32 @@ const Speakers = () => {
                 </span>
               </div>
             </div>
+            {/**Dr. Yogesh S. Chauhan (Section Chair) Department of Electrical Engineering, IIT Kanpur Email: chauhan@iitk.ac.in */}
+            <div className="col-span-1 shadow-md grid grid-cols-3 gap-3 bg-gray-100 rounded-lg">
+              <div className="col-span-1 w-full h-44">
+                <img
+                  src="https://res.cloudinary.com/dooi3sikb/image/upload/v1733296487/ConferenceAssets/uv2txzh5wpco4qluuf1e.jpg"
+                  className="h-full w-full object-cover rounded-lg "
+                  alt="Dr. Yogesh S. Chauhan"
+                />
+              </div>
+              <div className="flex flex-col gap-1 col-span-2 my-2">
+                <div className="py-2">
+                  <div className="rounded-full font-semibold w-fit bg-amber-600 bg-opacity-20">
+                    <p className="px-2 py-0.5 text-sm text-amber-800">
+                      National Speaker
+                    </p>
+                  </div>
+                </div>
+                <span className="font-semibold text-lg">
+                  Dr. Yogesh S. Chauhan
+                </span>
+                <span className="font-light text-xs pr-2">
+                  (Section Chair) IEEE UP Section, Department of Electrical
+                  Engineering, IIT Kanpur
+                </span>
+              </div>
+            </div>
             <div className="col-span-1 shadow-md grid grid-cols-3 gap-3 bg-gray-100 rounded-lg">
               <div className="col-span-1 w-full h-44">
                 <img
